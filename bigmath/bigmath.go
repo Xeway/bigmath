e2cbf3c856e11e9a02db5cac53d98358c5da8eb5
Ewan Humbert <xeway@protonmail.com>
2022-07-13 17:10:05 +0200

    Log10 for big.Float done

--- bigmath/bigmath.go
+++ bigmath/bigmath.go
@@ -50,23 +50,28 @@ func IntLog10(x *big.Int) float64 {
 	}
 }
 
-func FloatLog10(x *big.Int) float64 {
-	isOverflow := func(bigNum *big.Int) bool {
-		return bigNum.String() != strconv.Itoa(int(bigNum.Int64()))
+func FloatLog10(x *big.Float) float64 {
+	isOverflow := func(bigNum *big.Float) bool {
+		_, acc := bigNum.Float64()
+		if acc == big.Exact {
+			return false
+		} else {
+			return true
+		}
 	}
 
 	isOf := isOverflow(x)
 
 	if isOf {
-		num := make([]*big.Int, 0)
+		num := make([]*big.Float, 0)
 
 		firstIteration := true
 		for isOf {
 			if firstIteration {
-				num = append(num, big.NewInt(0).Sqrt(x))
+				num = append(num, big.NewFloat(0).Sqrt(x))
 				firstIteration = false
 			} else {
-				num = append(num, big.NewInt(0).Sqrt(num[len(num)-1]))
+				num = append(num, big.NewFloat(0).Sqrt(num[len(num)-1]))
 			}
 			isOf = isOverflow(num[len(num)-1])
 		}
@@ -80,7 +85,8 @@ func FloatLog10(x *big.Int) float64 {
 
 		for k := 0; k < int(numMul); k++ {
 			go func() {
-				bigNumLog += math.Log10(float64(num[len(num)-1].Int64()))
+				v, _ := num[len(num)-1].Float64()
+				bigNumLog += math.Log10(v)
 
 				wg.Done()
 			}()
@@ -89,6 +95,7 @@ func FloatLog10(x *big.Int) float64 {
 
 		return bigNumLog
 	} else {
-		return math.Log10(float64(x.Int64()))
+		v, _ := x.Float64()
+		return math.Log10(v)
 	}
 }
