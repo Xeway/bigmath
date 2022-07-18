ec2c116dc1f012d14e64492b5a6b6aeab0f0e18a
Ewan Humbert <xeway@protonmail.com>
2022-07-18 17:57:42 +0200

    Improved code readability

--- bigmath.go
+++ bigmath.go
@@ -14,40 +14,40 @@ func IntLog10(x *big.Int) float64 {
 
 	isOf := isOverflow(x)
 
-	if isOf {
-		num := make([]*big.Int, 0)
-
-		firstIteration := true
-		for isOf {
-			if firstIteration {
-				num = append(num, big.NewInt(0).Sqrt(x))
-				firstIteration = false
-			} else {
-				num = append(num, big.NewInt(0).Sqrt(num[len(num)-1]))
-			}
-			isOf = isOverflow(num[len(num)-1])
-		}
-
-		numMul := math.Pow(2, float64(len(num)))
-
-		var bigNumLog float64
-
-		var wg sync.WaitGroup
-		wg.Add(int(numMul))
-
-		for k := 0; k < int(numMul); k++ {
-			go func() {
-				bigNumLog += math.Log10(float64(num[len(num)-1].Int64()))
-
-				wg.Done()
-			}()
-		}
-		wg.Wait()
-
-		return bigNumLog
-	} else {
+	if !isOf {
 		return math.Log10(float64(x.Int64()))
 	}
+
+	num := make([]*big.Int, 0)
+
+	firstIteration := true
+	for isOf {
+		if firstIteration {
+			num = append(num, big.NewInt(0).Sqrt(x))
+			firstIteration = false
+		} else {
+			num = append(num, big.NewInt(0).Sqrt(num[len(num)-1]))
+		}
+		isOf = isOverflow(num[len(num)-1])
+	}
+
+	numMul := math.Pow(2, float64(len(num)))
+
+	var bigNumLog float64
+
+	var wg sync.WaitGroup
+	wg.Add(int(numMul))
+
+	for k := 0; k < int(numMul); k++ {
+		go func() {
+			bigNumLog += math.Log10(float64(num[len(num)-1].Int64()))
+
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+
+	return bigNumLog
 }
 
 func FloatLog10(x *big.Float) float64 {
@@ -62,40 +62,40 @@ func FloatLog10(x *big.Float) float64 {
 
 	isOf := isOverflow(x)
 
-	if isOf {
-		num := make([]*big.Float, 0)
-
-		firstIteration := true
-		for isOf {
-			if firstIteration {
-				num = append(num, big.NewFloat(0).Sqrt(x))
-				firstIteration = false
-			} else {
-				num = append(num, big.NewFloat(0).Sqrt(num[len(num)-1]))
-			}
-			isOf = isOverflow(num[len(num)-1])
-		}
-
-		numMul := math.Pow(2, float64(len(num)))
-
-		var bigNumLog float64
-
-		var wg sync.WaitGroup
-		wg.Add(int(numMul))
-
-		for k := 0; k < int(numMul); k++ {
-			go func() {
-				v, _ := num[len(num)-1].Float64()
-				bigNumLog += math.Log10(v)
-
-				wg.Done()
-			}()
-		}
-		wg.Wait()
-
-		return bigNumLog
-	} else {
+	if !isOf {
 		v, _ := x.Float64()
 		return math.Log10(v)
 	}
+
+	num := make([]*big.Float, 0)
+
+	firstIteration := true
+	for isOf {
+		if firstIteration {
+			num = append(num, big.NewFloat(0).Sqrt(x))
+			firstIteration = false
+		} else {
+			num = append(num, big.NewFloat(0).Sqrt(num[len(num)-1]))
+		}
+		isOf = isOverflow(num[len(num)-1])
+	}
+
+	numMul := math.Pow(2, float64(len(num)))
+
+	var bigNumLog float64
+
+	var wg sync.WaitGroup
+	wg.Add(int(numMul))
+
+	for k := 0; k < int(numMul); k++ {
+		go func() {
+			v, _ := num[len(num)-1].Float64()
+			bigNumLog += math.Log10(v)
+
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+
+	return bigNumLog
 }
