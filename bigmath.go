0bbcdb9b5af9b79510a65735a1ba427d632c43ee
Ewan Humbert <xeway@protonmail.com>
2022-07-24 21:02:26 +0200

    Improved IntLog10

--- bigmath.go
+++ bigmath.go
@@ -50,28 +50,19 @@ func IntLog10(x *big.Int) float64 {
 		return bigNum.String() != strconv.Itoa(int(bigNum.Int64()))
 	}
 
-	isOf := isOverflow(x)
-
-	if !isOf {
+	if !isOverflow(x) {
 		return math.Log10(float64(x.Int64()))
 	}
 
-	num := make([]*big.Int, 0)
+	x.Sqrt(x)
+	numMul := 2
 
-	firstIteration := true
-	for isOf {
-		if firstIteration {
-			num = append(num, big.NewInt(0).Sqrt(x))
-			firstIteration = false
-		} else {
-			num = append(num, big.NewInt(0).Sqrt(num[len(num)-1]))
-		}
-		isOf = isOverflow(num[len(num)-1])
+	for isOverflow(x) {
+		x.Sqrt(x)
+		numMul = numMul << 1
 	}
 
-	numMul := math.Pow(2, float64(len(num)))
-
-	bigNumLog := math.Log10(float64(num[len(num)-1].Int64())) * numMul
+	bigNumLog := math.Log10(float64(x.Int64())) * float64(numMul)
 
 	return bigNumLog
 }
